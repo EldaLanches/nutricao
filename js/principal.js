f36f632cf321a6aab99d87984ee4238457becc5d
Eloá <eloa.casarotto@escola.pr.gov.br>
2025-10-07 09:02:21 -0300

    yj

--- js/principal.js
+++ js/principal.js
@@ -1,7 +1,7 @@
 var titulo = document.querySelector("h1");
 console.log(titulo);
 console.log(titulo.textContent);
-titulo.textContent = ("Elda Nutri");
+titulo.textContent = ("Aparecida Nutrição");
 
 var paciente = document.querySelector("#primeiro-paciente");
 console.log(paciente);
