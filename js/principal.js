e3dbea1db479a0b7407cc65467dd016e1053864d
Eloá <eloa.casarotto@escola.pr.gov.br>
2025-10-10 10:32:44 -0300

    hfg

--- js/principal.js
+++ js/principal.js
@@ -22,17 +22,20 @@ console.log(imc);
 var pesoEhValido = true;
 var alturaEhValido = true;
 
-if (peso < 0 || peso > 1000){
+if (peso <= 0 || peso >= 1000){
     console.log("Peso inválido!");
     pesoEhValido = false;
+    tdImc.textContent = "Peso inválido!";
 }
 
-if(altura < 0 || altura > 3.00){
+if(altura <= 0 || altura >= 3.00){
      console.log("Altura inválida!");
      alturaEhValido = false;
+     tdImc.textContent = "Altura inválida!";
 }
 
 if(alturaEhValida && pesoEhValido){
     var imc = peso / (altura * altura);
     tdImc.textContent = imc;
 }
+
