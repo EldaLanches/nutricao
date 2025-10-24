14244a7d271b6c860bc40c547313e59265ed236f
Eloá <eloa.casarotto@escola.pr.gov.br>
2025-10-24 10:01:18 -0300

    vcbcv

--- js/principal.js
+++ js/principal.js
@@ -36,6 +36,6 @@ if (altura <= 0 || altura >= 3.00){
 
 if (alturaEhValido && pesoEhValido){
     var imc = peso / (altura * altura);
-    tdImc.textContent = imc;
+    tdImc.textContent = imc.toFixed(2);
 }
 
