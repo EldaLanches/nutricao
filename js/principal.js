e4533a4947446aa04eea3ebacf8635a224725abf
Eloá <eloa.casarotto@escola.pr.gov.br>
2025-10-28 09:42:41 -0300

    dsad

--- js/principal.js
+++ js/principal.js
@@ -43,5 +43,5 @@ for (var i = 0; i < pacientes.length; i++) {
 
 titulo.addEventListener("click", mostraMensagem);
 function mostraMensagem(){
-    alert("Tá me catucando pq?!?")
+    alert("Olá, eu fui clicado em uma função anônima")
 }
